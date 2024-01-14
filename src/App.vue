11b525e3f3729c7288ffb26b45fd427f9223dc80
alex-sly <sly1521@mail.ru>
2024-01-14 15:22:53 +0500

    lesson02

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app">HelloWorld</div>
+  <div class="wrapper">
+    <hero-view />
+
+    <footer-component />
+  </div>
 </template>
 
 <script>
-export default {};
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: { HeroView, FooterComponent },
+};
 </script>
 
 <style></style>
