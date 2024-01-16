47c1be9637a887b925c0dcbcfd6b9f1e93f56517
alex-sly <sly1521@mail.ru>
2024-01-16 22:37:58 +0500

    lesson04

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import VueRouter from "vue-router";
 
 import HeroView from "../views/HeroView";
 import OurCoffeeView from "../views/OurCoffeeView";
+import ForYouPleasureView from "../views/ForYouPleasureView";
+import ContactUsView from "../views/ContactUsView";
+import ThankYouView from "../views/ThankYouView";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: HeroView },
   { path: "/our-coffee", component: OurCoffeeView },
+  { path: "/for-you-pleasure", component: ForYouPleasureView },
+  { path: "/contact-us", component: ContactUsView },
+  { path: "/thank-you", component: ThankYouView },
 ];
 
 const router = new VueRouter({
